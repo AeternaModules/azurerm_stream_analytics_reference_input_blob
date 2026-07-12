700b9254418a35fc3191c68e2c4e3a22d3c60045
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:33 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "storage_account_key" {
+  for_each     = { for k, v in var.stream_analytics_reference_input_blobs : k => v if v.storage_account_key_key_vault_id != null && v.storage_account_key_key_vault_secret_name != null }
+  name         = each.value.storage_account_key_key_vault_secret_name
+  key_vault_id = each.value.storage_account_key_key_vault_id
+}
 resource "azurerm_stream_analytics_reference_input_blob" "stream_analytics_reference_input_blobs" {
   for_each = var.stream_analytics_reference_input_blobs
 
@@ -10,7 +15,7 @@ resource "azurerm_stream_analytics_reference_input_blob" "stream_analytics_refer
   stream_analytics_job_name = each.value.stream_analytics_job_name
   time_format               = each.value.time_format
   authentication_mode       = each.value.authentication_mode
-  storage_account_key       = each.value.storage_account_key
+  storage_account_key       = each.value.storage_account_key != null ? each.value.storage_account_key : try(data.azurerm_key_vault_secret.storage_account_key[each.key].value, null)
 
   serialization {
     encoding        = each.value.serialization.encoding
--- variables.tf
+++ variables.tf
@@ -17,19 +17,23 @@ Required:
 Optional:
     - authentication_mode
     - storage_account_key
+    - storage_account_key_key_vault_id (alternative to storage_account_key - read from Key Vault instead)
+    - storage_account_key_key_vault_secret_name (alternative to storage_account_key - read from Key Vault instead)
 EOT
 
   type = map(object({
-    date_format               = string
-    name                      = string
-    path_pattern              = string
-    resource_group_name       = string
-    storage_account_name      = string
-    storage_container_name    = string
-    stream_analytics_job_name = string
-    time_format               = string
-    authentication_mode       = optional(string) # Default: "ConnectionString"
-    storage_account_key       = optional(string)
+    date_format                               = string
+    name                                      = string
+    path_pattern                              = string
+    resource_group_name                       = string
+    storage_account_name                      = string
+    storage_container_name                    = string
+    stream_analytics_job_name                 = string
+    time_format                               = string
+    authentication_mode                       = optional(string) # Default: "ConnectionString"
+    storage_account_key                       = optional(string)
+    storage_account_key_key_vault_id          = optional(string)
+    storage_account_key_key_vault_secret_name = optional(string)
     serialization = object({
       encoding        = optional(string)
       field_delimiter = optional(string)
